3449fd434641aaba2a18d0d839833faf476ace1f
Reece Hagan <drogonmar@gmail.com>
2020-07-17 15:54:10 -0700

    Added Zomo.Core Core classes needed for Zomo.

---
+++ Zomo.Core/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zomo.Core
+{
+    /*
+     * A simple class that you construct with a file path.
+     * You can store and retrieve vars from. 
+     */
+    public class Config
+    {
+        
+    }
+}
